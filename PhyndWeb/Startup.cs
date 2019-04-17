8fb08908c27d35917086ce7784f3e84f857c6850
halomademeapc <opr9029@gmail.com>
2019-04-16 21:35:19 -0400

    Set up configuration

--- PhyndWeb/Startup.cs
+++ PhyndWeb/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PhyndData;
@@ -12,6 +13,13 @@ namespace PhyndWeb
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +28,8 @@ namespace PhyndWeb
                 .AddNewtonsoftJson();
 
             services.AddRazorComponents();
+
+            services.Configure<LearningConfiguration>(Configuration.GetSection("Learning"));
 
             services.AddSingleton<WeatherForecastService>();
             services.AddScoped<MoveService>();
